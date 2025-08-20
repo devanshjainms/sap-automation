a33edcb63e16689f108bfa0e27cc230d2a2c3ac7
devanshjain <devanshjain@microsoft.com>
2025-08-20 21:52:46 +0000

    Fix client_secret references to use data block instead of ephemeral in azurerm provider configurations

--- deploy/terraform/run/sap_backup/providers.tf
+++ deploy/terraform/run/sap_backup/providers.tf
@@ -26,7 +26,7 @@ provider "azurerm"            {
                                 }
                                 subscription_id            = length(var.subscription_id) > 0 ? var.subscription_id : data.azurerm_key_vault_secret.subscription_id[0].value
                                 client_id                  = var.use_spn ? data.azurerm_key_vault_secret.client_id[0].value: null
-                                client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.client_secret[0].value: null
+                                client_secret              = var.use_spn ? data.azurerm_key_vault_secret.client_secret[0].value: null
                                 tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value: null
                                 use_msi                    = var.use_spn ? false : true
                                 storage_use_azuread        = true
@@ -39,7 +39,7 @@ provider "azurerm"            {
                                 alias                      = "deployer"
                                 subscription_id            = coalesce(var.management_dns_subscription_id, local.SAPLibrary_subscription_id, length(local.deployer_subscription_id) > 0 ? local.deployer_subscription_id : "", local.SAPLibrary_subscription_id)
                                 client_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_client_id[0].value : null
-                                client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.cp_client_secret[0].value : null
+                                client_secret              = var.use_spn ? data.azurerm_key_vault_secret.cp_client_secret[0].value : null
                                 tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.cp_tenant_id[0].value: null
                                 use_msi                    = var.use_spn ? false : true
                                 storage_use_azuread        = true
@@ -78,7 +78,7 @@ provider "azurerm"            {
 
 provider "azuread"            {
                                 client_id                  = var.use_spn ? data.azurerm_key_vault_secret.client_id[0].value: null
-                                client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.client_secret[0].value: null
+                                client_secret              = var.use_spn ? data.azurerm_key_vault_secret.client_secret[0].value: null
                                 tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value: null
                                 use_msi                    = var.use_spn ? false : true
                               }
@@ -87,7 +87,7 @@ provider "azapi"              {
                                 alias                      = "api"
                                 subscription_id            = length(var.subscription_id) > 0 ? var.subscription_id : data.azurerm_key_vault_secret.subscription_id[0].value
                                 client_id                  = var.use_spn ? data.azurerm_key_vault_secret.client_id[0].value: null
-                                client_secret              = var.use_spn ? ephemeral.azurerm_key_vault_secret.client_secret[0].value: null
+                                client_secret              = var.use_spn ? data.azurerm_key_vault_secret.client_secret[0].value: null
                                 tenant_id                  = var.use_spn ? data.azurerm_key_vault_secret.tenant_id[0].value: null
                                 use_msi                    = var.use_spn ? false : true
                               }

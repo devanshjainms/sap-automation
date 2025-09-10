3709b957577fef38cfb576dcdb2111fd17408c1b
devanshjain <devanshjain@microsoft.com>
2025-09-10 20:51:01 +0000

    fix: update remote execution method in HANA installation response file and correct variable update command in keyvault script

--- deploy/scripts/pipeline_scripts/v1/00-store-secrets-in-keyvault.sh
+++ deploy/scripts/pipeline_scripts/v1/00-store-secrets-in-keyvault.sh
@@ -101,7 +101,7 @@ if [ -v PARENT_VARIABLE_GROUP ]; then
 		if [ -z "$WZ_DEPLOYER_KEYVAULT" ]; then
 			az pipelines variable-group variable create --group-id "${VARIABLE_GROUP_ID}" --name "DEPLOYER_KEYVAULT" --value "$DEPLOYER_KEYVAULT" --output none
 		else
-			az pipelines variable-group variable update --group-id "${VARIABLE_GROUP_ID}" --name "DEPLOYER_KEYVAULT" --value "$DEPLOYER_KEYVAULT" --output none
+			az pipelines variable-group variable update --group-id "${VARIABLE_GROUP_ID}" --name "DEPLOYER_KEYVAULT" --new-value "$DEPLOYER_KEYVAULT" --output none
 		fi
 
 		export PARENT_VARIABLE_GROUP_ID
